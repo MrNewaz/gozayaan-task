d1b446520ed68d4d734d4db6209bc8f045524a15
Saif Rahman <saif.rahman@udplatforms.com>
2023-11-22 16:13:37 +0600

    Added material theme and custom fonts

---
+++ src/Theme.jsx
@@ -0,0 +1,34 @@
+import { CssBaseline } from "@mui/material"
+import {
+  createTheme,
+  responsiveFontSizes,
+  ThemeProvider,
+} from "@mui/material/styles"
+import App from "./App"
+
+const Theme = () => {
+  const theme = createTheme({
+    palette: {
+      mode: "light",
+      background: {
+        default: "#430099",
+        card: "#FFF",
+      },
+      text: {
+        primary: "#111",
+        secondary: "#1F1F23",
+      },
+    },
+    typography: {
+      fontFamily: "Helvetica Neue",
+    },
+  })
+  return (
+    <ThemeProvider theme={responsiveFontSizes(theme)}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  )
+}
+
+export default Theme
